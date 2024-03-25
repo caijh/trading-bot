760093c9ebbe2ad96b41b6ba6ff196767724f6c3
johnhuitsai <caiqizhe@gmail.com>
2024-03-25 23:28:17 +0800

    Refactor main function, extract server related logic to a separate module `server`, add `register` async function for service registration, and update dependencies

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,17 @@
 use async_trait::async_trait;
 use axum::Router;
 use config::Config;
+use database::DbService;
 use logger::{Logger, LoggerConfig};
+use state::TypeMap;
 use web::bootstrap::Bootstrap;
 use web::health::health_routers;
 
 pub struct StockBotServer;
+
+lazy_static::lazy_static! {
+    pub static ref SERVICES: TypeMap![Send + Sync] = <TypeMap![Send + Sync]>::new();
+}
 
 #[async_trait]
 impl Bootstrap for StockBotServer {
@@ -13,6 +19,9 @@ impl Bootstrap for StockBotServer {
         // set logger.
         let logger_config = LoggerConfig::get_config(config);
         Logger::init_logger(&logger_config);
+
+        let database_service = DbService::create(config).await;
+        SERVICES.set(database_service);
     }
 
     async fn init_routes(&self, router: Router) -> Router {
