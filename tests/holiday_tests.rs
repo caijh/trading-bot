95a26895c3fa0b42bd3ae70c8daa3dd00ce73861
蔡俊辉 <caiqizhe@gmail.com>
2024-05-04 08:28:36 +0800

    Update tests: Add holiday tests for stock-bot

---
+++ tests/holiday_tests.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    use stock_bot::holiday_api::get_holidays;
+
+    #[tokio::test]
+    async fn test_get_holidays() {
+        let dates = get_holidays().await.unwrap();
+        assert_eq!(true, dates.len() > 0);
+    }
+}
