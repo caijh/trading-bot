39e83bddfb7d182967844dd21bc5bd9a1c21779b
蔡俊辉(10039330) <caijh@glodon.com>
2024-05-06 14:43:14 +0800

    Add cross star stock pattern

--- src/stock_pattern.rs
+++ src/stock_pattern.rs
@@ -1,27 +1,42 @@
+use std::str::FromStr;
 use bigdecimal::BigDecimal;
+use rbatis::rbdc::Decimal;
+
 use crate::stock::StockDailyPrice;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum StockPattern {
     /// 长下影线
     LongLowerShadow,
+    /// 十字星
+    CrossStar,
     UnKnown,
 }
 
 pub fn get_stock_pattern(price: &StockDailyPrice) -> StockPattern {
-    let open = price.open.clone();
-    let close = price.close.clone();
-    let low = price.low.clone();
+    let open = &price.open;
+    let close = &price.close;
+    let low = &price.low;
     if open <= close {
-        let p = (low - open.clone()).abs() / (open - close).abs();
+        let p = (low.clone() - open.clone()).abs() / (open.clone() - close.clone()).abs();
 
-        if p >= BigDecimal::from(2) {
+        if p >= BigDecimal::from_str("2").unwrap() {
             return StockPattern::LongLowerShadow;
         }
+        
+        let p: Decimal = open.clone() / close.clone();
+        if p >= Decimal::new("0.999").unwrap() {
+            return StockPattern::CrossStar;
+        }
     } else {
-        let p = (low - close.clone()).abs() / (close - open).abs();
-        if p >= BigDecimal::from(2)  {
+        let p = (low.clone() - close.clone()).abs() / (close.clone() - open.clone()).abs();
+        if p >= BigDecimal::from_str("2").unwrap() {
             return StockPattern::LongLowerShadow;
+        }
+        
+        let p: Decimal = close.clone() / open.clone();
+        if p >= Decimal::new("0.999").unwrap() {
+            return StockPattern::CrossStar;
         }
     }
     StockPattern::UnKnown
--- tests/stock_pattern_tests.rs
+++ tests/stock_pattern_tests.rs
@@ -22,6 +22,23 @@ mod tests {
             zde: None,
         };
         let pattern = get_stock_pattern(&price);
-        assert_eq!(StockPattern::LongLowerShadow, pattern)
+        assert_eq!(StockPattern::LongLowerShadow, pattern);
+
+        let price = StockDailyPrice {
+            code: "600916".to_string(),
+            high: Decimal::new("11.26").unwrap(),
+            low: Decimal::new("11.24").unwrap(),
+            open: Decimal::new("11.25").unwrap(),
+            amount: Some(Decimal::new("300").unwrap()),
+            volume: None,
+            zf: None,
+            hs: None,
+            zd: None,
+            date: 0,
+            close:  Decimal::new("11.26").unwrap(),
+            zde: None,
+        };
+        let pattern = get_stock_pattern(&price);
+        assert_eq!(StockPattern::CrossStar, pattern);
     }
 }
