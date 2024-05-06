45d780a502c293b5e1b5c65e7b501d1b67faac49
蔡俊辉(10039330) <caijh@glodon.com>
2024-05-06 11:08:36 +0800

    Update data fields in StockDailyPrice and get_stock_pattern function

--- src/stock_pattern.rs
+++ src/stock_pattern.rs
@@ -8,9 +8,9 @@ pub enum StockPattern {
 }
 
 pub fn get_stock_pattern(price: &StockDailyPrice) -> StockPattern {
-    let open = price.open.clone().unwrap();
-    let close = price.close.clone().unwrap();
-    let low = price.low.clone().unwrap();
+    let open = price.open.clone();
+    let close = price.close.clone();
+    let low = price.low.clone();
     if open <= close {
         let p = (low - open.clone()).abs() / (open - close).abs();
 
