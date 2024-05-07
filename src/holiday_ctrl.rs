19a2f262c5a19225dd84ca801dcff88f124daf0f
蔡俊辉(10039330) <caijh@glodon.com>
2024-05-07 14:13:47 +0800

    Chore comments

--- src/holiday_ctrl.rs
+++ src/holiday_ctrl.rs
@@ -12,6 +12,13 @@ pub fn holiday_routers() -> Router {
         .route("/today", get(today_is_holiday))
 }
 
+/// `today_is_holiday`是一个公共异步函数
+///
+/// 此函数检查今天是否为假日。如果今天是假日，返回`true`；否则，返回`false`.
+///
+/// 这个函数首先获取当前的本地时间，然后用`is_holiday`函数来检查这个日期是否为假日。
+///
+/// 最后，将结果封装在`RespBody`中，并作为响应返回。
 pub async fn today_is_holiday() -> impl IntoResponse {
     let now = Local::now();
     let r = is_holiday(&now).await;
@@ -19,6 +26,9 @@ pub async fn today_is_holiday() -> impl IntoResponse {
     RespBody::from_result(&r).response()
 }
 
+/// 定义一个异步函数sync，返回类型为IntoResponse的实现
+/// 该函数首先调用sync_holidays异步方法获取数据
+/// 然后将结果转换为RespBody，并构建一个响应对象
 pub async fn sync() -> impl IntoResponse {
     let r = sync_holidays().await;
 
