83ed8bfe2c783c84048f6803c1f7ca4da623b328
蔡俊辉(10039330) <caijh@glodon.com>
2024-05-07 15:30:40 +0800

    Refactored code in stock_index_svc.rs to improve efficiency and code reusability.

--- src/stock_index_svc.rs
+++ src/stock_index_svc.rs
@@ -7,23 +7,16 @@ use crate::stock_index::{IndexConstituent, StockIndex};
 use crate::stock_index_api;
 
 pub async fn get_constituent_stocks(index: &str) -> Result<Vec<IndexConstituent>, Box<dyn Error>> {
+    let index = get_stock_index(index).await?;
     let dao = SERVICES.get::<DbService>().dao();
-    let index = StockIndex::select_by_code(dao, index).await?;
-    if index.is_none() {
-        return Err("Stock index is no Supported".into());
-    }
-    let index = index.unwrap();
     let stocks = IndexConstituent::select_by_column(dao, "index_code", &index.code).await?;
     Ok(stocks)
 }
 
 pub async fn sync_constituents(index: &str) -> Result<(), Box<dyn Error>> {
+    let index = get_stock_index(index).await?;
+
     let dao = SERVICES.get::<DbService>().dao();
-    let index = StockIndex::select_by_code(dao, index).await?;
-    if index.is_none() {
-        return Err("Stock index is no Supported".into());
-    }
-    let index = index.unwrap();
     let stocks = stock_index_api::get_stocks(&index.code, &index.exchange).await?;
     IndexConstituent::delete_by_column(dao, "index_code", &index.code).await?;
     let mut constituents = Vec::new();
@@ -37,4 +30,13 @@ pub async fn sync_constituents(index: &str) -> Result<(), Box<dyn Error>> {
     }
     IndexConstituent::insert_batch(dao, &constituents, constituents.len() as u64).await?;
     Ok(())
+}
+
+pub async fn get_stock_index(index: &str) -> Result<StockIndex, Box<dyn Error>> {
+    let dao = SERVICES.get::<DbService>().dao();
+    let index = StockIndex::select_by_code(dao, index).await?;
+    match index {
+        None => Err("Stock index is no Supported".into()),
+        Some(index) => Ok(index),
+    }
 }
