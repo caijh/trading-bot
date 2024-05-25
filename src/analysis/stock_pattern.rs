b3cc322f7c4e882dce18b6204cb377c3b4ac0609
johnhuitsai <caiqizhe@gmail.com>
2024-05-25 19:20:31 +0800

    Refactor stock pattern analysis

--- src/analysis/stock_pattern.rs
+++ src/analysis/stock_pattern.rs
@@ -23,39 +23,39 @@ pub fn get_stock_pattern(price: &StockDailyPrice) -> StockPattern {
     let high = &price.high;
     let factor = BigDecimal::from_str("1.2").unwrap();
     if open <= close {
-        let mut m = close.clone() - open.clone();
-        if m == Decimal::new("0").unwrap() {
-            m = Decimal::new("1").unwrap();
+        let mut real_body = close.clone() - open.clone();
+        if real_body == Decimal::new("0").unwrap() {
+            real_body = Decimal::new("1").unwrap();
         }
-        let sub1 = (low.clone() - open.clone()).abs();
-        let sub2 = (close.clone() - high.clone()).abs();
+        let lower_shadow = (low.clone() - open.clone()).abs();
+        let upper_shadow = (close.clone() - high.clone()).abs();
 
-        let p = sub1.clone() / m.abs();
+        let p = lower_shadow.clone() / real_body.abs();
         // 下影线长度是实体长度的2倍并且下影线长度要大于上影线长度
-        if p > BigDecimal::from_str("2").unwrap() && sub1 >= sub2.clone() * factor {
+        if p > BigDecimal::from_str("2").unwrap() && lower_shadow >= upper_shadow.clone() * factor {
             return StockPattern::LongLowerShadow;
         }
 
         let p: Decimal = open.clone() / close.clone();
-        if p > Decimal::new("0.999").unwrap() && sub1 >= sub2 {
+        if p > Decimal::new("0.999").unwrap() && lower_shadow >= upper_shadow {
             return StockPattern::CrossStar;
         }
     } else {
-        let mut m = open.clone() - close.clone();
-        if m == Decimal::new("0").unwrap() {
-            m = Decimal::new("1").unwrap();
+        let mut real_body = open.clone() - close.clone();
+        if real_body == Decimal::new("0").unwrap() {
+            real_body = Decimal::new("1").unwrap();
         }
-        let sub1 = (low.clone() - close.clone()).abs();
-        let sub2 = (open.clone() - high.clone()).abs();
+        let lower_shadow = (low.clone() - close.clone()).abs();
+        let upper_shadow = (open.clone() - high.clone()).abs();
 
-        let p = sub1.clone() / m.abs();
+        let p = lower_shadow.clone() / real_body.abs();
         // 下影线长度是实体长度的2倍并且下影线长度要大于上影线长度
-        if p > BigDecimal::from_str("2").unwrap() && sub1 >= sub2.clone() * factor {
+        if p > BigDecimal::from_str("2").unwrap() && lower_shadow >= upper_shadow.clone() * factor {
             return StockPattern::LongLowerShadow;
         }
 
         let p: Decimal = close.clone() / open.clone();
-        if p > Decimal::new("0.999").unwrap() && sub1 >= sub2 {
+        if p > Decimal::new("0.999").unwrap() && lower_shadow >= upper_shadow {
             return StockPattern::CrossStar;
         }
     }
