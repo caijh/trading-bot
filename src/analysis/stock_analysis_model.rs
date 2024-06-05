c8b5d93e0ae20f52b24a6e2597753467567a415b
蔡俊辉(10039330) <caijh@glodon.com>
2024-06-05 08:58:22 +0800

    Add Comment for AnalyzedStock struct in stock_analysis_model

--- src/analysis/stock_analysis_model.rs
+++ src/analysis/stock_analysis_model.rs
@@ -2,9 +2,13 @@ use serde::{Deserialize, Serialize};
 
 use crate::analysis::stock_pattern::StockPattern;
 
+/// AnalyzedStock 结构体代表一只经过分析的股票，包含股票的基本信息和技术分析模式。
 #[derive(Serialize, Deserialize, Clone)]
 pub struct AnalyzedStock {
+    /// 股票代码
     pub code: String,
+    /// 股票名称。
     pub name: String,
+    /// 股票的技术分析模式，用于描述股票价格走势的特定模式。
     pub pattern: StockPattern,
 }
