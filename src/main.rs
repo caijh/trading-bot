3aadf6902103ff8093c1da3634ece247bb0c827a
johnhuitsai <caiqizhe@gmail.com>
2024-08-12 00:00:18 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
 use std::error::Error;
 
+use application::application::{Application, RustApplication};
 use clap::Parser;
 use cli::Cli;
+use database::DbService;
 use registration::{deregister, register};
+use tokio::runtime::Runtime;
 use web::bootstrap::Bootstrap;
 
+use trading_bot::job::jobs::load_jobs;
 use trading_bot::server::StockBotServer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let application = RustApplication::default();
+
+    application
+        .publisher
+        .subscribe(ApplicationEvent::Prepared, |application| {
+            let db_service = Runtime::new()
+                .unwrap()
+                .block_on(DbService::create(&application.config));
+            application.context.set(db_service);
+        });
+
+    application
+        .publisher
+        .subscribe(ApplicationEvent::ContextInited, |_application| {
+            Runtime::new()
+                .unwrap()
+                .block_on(load_jobs())
+                .expect("Fail to load jobs");
+        });
+
+    application.run()?;
+
     let cli: Cli = Cli::parse();
 
     let server = StockBotServer;
