e2d6affe421e00f32ed74fe7acf85c2cc875e43c
johnhuitsai <caiqizhe@gmail.com>
2024-10-08 22:13:59 +0800

    Replace RespBody::from_result with RespBody::result

--- src/index/stock_index_ctrl.rs
+++ src/index/stock_index_ctrl.rs
@@ -9,12 +9,12 @@ use crate::index::stock_index_svc;
 pub async fn get_stocks(Path(code): Path<String>) -> impl IntoResponse {
     let r = stock_index_svc::get_constituent_stocks(&code).await;
 
-    RespBody::from_result(&r).response()
+    RespBody::result(&r).response()
 }
 
 #[get("/index/sync/:code")]
 pub async fn sync(Path(code): Path<String>) -> impl IntoResponse {
     let r = stock_index_svc::sync_constituents(&code).await;
 
-    RespBody::from_result(&r).response()
+    RespBody::result(&r).response()
 }
