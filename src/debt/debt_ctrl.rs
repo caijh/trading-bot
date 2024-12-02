09cfcfc495522cd43855533522b152b36315f65d
johnhuitsai <caiqizhe@gmail.com>
2024-12-02 15:12:45 +0800

    refactor(debt): 重构国债逆回购价格信息获取接口
    - 更新了 DebtParams 结构体的命名，以更准确地反映其用途
    - 添加了 get_debt_price 函数的文档注释，详细说明了其功能、参数和返回值
    - 优化了代码格式和结构，提高了可读性

--- src/debt/debt_ctrl.rs
+++ src/debt/debt_ctrl.rs
@@ -7,12 +7,22 @@ use serde::{Deserialize, Serialize};
 use crate::debt::debt_svc;
 
 #[derive(Serialize, Deserialize)]
-struct Params {
+struct DebtParams {
     code: String,
 }
 
+/// 获取债务价格信息
+///
+/// 该函数通过HTTP GET请求获取国债逆回购价格信息，请求路径为"/debt/price"
+/// 使用`Query`提取请求参数，参数类型为`DebtParams`
+///
+/// 参数:
+/// - Query(params): 从请求中提取的国债逆回购参数，用于查询特定债务的价格信息
+///
+/// 返回:
+/// - impl IntoResponse: 返回一个实现了`IntoResponse` trait的响应对象
 #[get("/debt/price")]
-async fn get_debt_price(Query(params): Query<Params>) -> impl IntoResponse {
+async fn get_debt_price(Query(params): Query<DebtParams>) -> impl IntoResponse {
     let r = debt_svc::get_debt_price(&params.code).await;
     RespBody::result(&r).response()
 }
