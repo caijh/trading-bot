c34a3d95aea3f697f710958d74e65680cd215acb
johnhuitsai <caiqizhe@gmail.com>
2025-04-04 12:09:58 +0800

    init

---
+++ src/test/java/com/github/caijh/apps/trading/bot/TradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caijh.apps.trading.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
