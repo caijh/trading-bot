4fe3370ca2b6895a7963c3f4aa28fabdcf1b89bd
johnhuitsai <caiqizhe@gmail.com>
2025-04-07 22:10:25 +0800

    Update TradingDataFeignClient to include URL in @FeignClient annotation.

--- src/main/java/com/github/caijh/apps/trading/bot/feign/TradingDataFeignClient.java
+++ src/main/java/com/github/caijh/apps/trading/bot/feign/TradingDataFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "trading-data")
+@FeignClient(name = "trading-data", url = "https://gateway.joinspace.pp.ua/trading-data")
 public interface TradingDataFeignClient {
 
     @GetMapping(value = "/stock/price")
