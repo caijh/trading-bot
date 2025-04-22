92a079759d6a7e95cc10b80c7a3a42d2fa20e025
johnhuitsai <caiqizhe@gmail.com>
2025-04-22 19:26:42 +0800

    feat(trading-bot): 添加 Feign 客户端的重试功能
    - 在 TradingDataFeignClient 接口中，为 getPrice 和 getMarketStatus 方法添加了 @Retryable 注解
    - 这将允许在调用这些方法时，如果遇到临时性错误，能够自动进行重试
    - 重试策略将使用 Spring Retry 框架的默认配置

--- src/main/java/com/github/caijh/apps/trading/bot/feign/TradingDataFeignClient.java
+++ src/main/java/com/github/caijh/apps/trading/bot/feign/TradingDataFeignClient.java
@@ -3,6 +3,7 @@ package com.github.caijh.apps.trading.bot.feign;
 import com.github.caijh.apps.trading.bot.dto.ApiResponse;
 import com.github.caijh.apps.trading.bot.dto.StockPrice;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface TradingDataFeignClient {
 
     @GetMapping(value = "/stock/price")
+    @Retryable
     ApiResponse<StockPrice> getPrice(@RequestParam String code);
 
     @GetMapping(value = "/exchange/{exchange}/market/status")
+    @Retryable
     ApiResponse<String> getMarketStatus(@PathVariable String exchange);
 
 }
